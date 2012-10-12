389acdd410bc793a93a92e40d47907b9da3813eb
Herman Moreno <herman.moreno@crowdint.com>
2012-10-12 17:06:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instafood::Application.config.secret_token = '73797a962d1bf26a9f16b1624cd0c228b632765c334f546c540540ddeb3bfb7539f1c4dbc7910b39dac35d707cd01139512f91f42c6f78a9726031e564987add'
