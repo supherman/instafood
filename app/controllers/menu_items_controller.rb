6b9506efe1f70af7c3d3a19c6800f389cc974247
BlackLocus Pair2 <blacklocus@crowdint.com>
2012-10-14 04:42:39 -0500

    Include variations in the menu items controller responses

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -4,11 +4,11 @@ class MenuItemsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with MenuItem.all
+    respond_with MenuItem.includes(:variations), include: :variations
   end
 
   def show
-    respond_with @menu_item
+    respond_with @menu_item, include: :variations
   end
 
   def create
@@ -16,11 +16,11 @@ class MenuItemsController < ApplicationController
   end
 
   def update
-    respond_with @menu_item.update_attributes(params[:menu_item])
+    respond_with @menu_item.update_attributes(params[:menu_item]), include: :variations
   end
 
   def destroy
-    respond_with @menu_item.destroy
+    respond_with @menu_item.destroy, include: :variations
   end
 
   private
